a62d65cc5edac3f3a0514e037c834db114e40432
Shux_DEV <70896657+PonchHKT@users.noreply.github.com>
2021-12-09 10:18:04 +0100

    updating

--- frontend/composants/ChatContainer.js
+++ frontend/composants/ChatContainer.js
@@ -1,23 +1,83 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/ChatContainer.module.scss'
 
-const ChatContainer = () => {
+const ChatContainer = (props) => {
+
+    const [msgList, setMsgList] = useState([])
+
+    useEffect(() => {
+        console.log(props.socket)
+        
+    }, [props.socket])
+    
+    if(props.socket !== undefined) {
+        props.socket.on("chat message", (data) => {
+            const tab = [...msgList]
+            tab.push({ user: data.nick, message: data.message })
+            setMsgList(tab)
+            console.log(msgList)
+        });
+    }
+
+    const addNewMessage = () => {
+        const inputField = document.querySelector('#input-send-msg')
+
+        props.socket.emit("chat message", {
+            message: inputField.value,
+        });
+    }                    
+
+    console.log(msgList.length)
+
     return (
-        <div>
-            <div className={styles.chatbox}>
-            <div className={styles.chatwindow}>
-            <div style={{position: 'absolute', bottom: 0,}}>
-            <div className={styles.chatinput}>
-            <input className={styles.input} type="text" placeholder="Aa"/>
-            <button className={styles.button} type="submit">
-            <i style={{fontSize: 30, }} class="far fa-link"/>
-            <i style={{fontSize: 30, }} class="fad fa-paper-plane"/>
-            <i style={{fontSize: 30, }} class="far fa-icons"/>
-            </button>
+        <div className={styles.chatContainer}>
+            <div className={styles.chatContainerHeader}>
+                <div className={styles.action}>
+                    <i className="fad fa-search"></i>
+                    <i className="fad fa-heart"></i>
+                    <i className="fad fa-bell"></i>
+                </div>
             </div>
+
+            <div className={styles.chatContainerMsg}>
+
+                <div className={styles.msg}>
+                    </div>
+
+                <div className={styles.msg}>
+                    <div className={styles.date}>
+                        <hr />
+                        <p>Bienvenue !</p>
+                        <hr/>
+                    </div>
+
+                    {   
+                        msgList.length >= 1 ?
+
+                            msgList.map((x) => {
+                                console.log(x.message)
+                                return (
+                                    
+                                    <p className={x.user === props.socket.id ? styles.right : styles.left}>{x.message}</p>
+                                    
+                                )
+                            })
+                        :
+                            <p>ECRIRE SON PREMIER MESSAGE</p>
+                    }
+
+                </div>
             </div>
+
+            <div className={styles.chatContainerSend}>
+                <i className="fad fa-paperclip"></i>
+                <i className="fad fa-camera-alt"></i>
+                <i className="fad fa-smile-wink"></i>
+                <textarea id="input-send-msg" placeholder="Salut, ça va ?" type="text"/>
+                <i onClick={() => addNewMessage()} className="fad fa-arrow-circle-right"></i>
+            </div>
+
         </div>
-    </div>
-</div>
     )
 }
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,30 @@
 import ChatContainer from '../composants/ChatContainer'
 import ProfilContainer from '../composants/ProfilContainer'
 import SidebarContainer from '../composants/SidebarContainer'
+import { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
 
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+
+    const [socketId, setSocketId] = useState()
+
+    useEffect(() => {
+        const socket = io("ws://localhost:4242");
+        setSocketId(socket)
+
+        socket.on("new_msg", (data) => {
+            alert(data.msg);
+        })
+    }, [])
+
     return (
         <div className={styles.homeContainer}>
       
             <SidebarContainer />
 
-            <ChatContainer />
+            <ChatContainer socket={socketId}/>
 
             <ProfilContainer />
 
